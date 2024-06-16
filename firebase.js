1096dafc28f8c3dbfd8132db74a3c463628ebf72
alex-salnikov <alexander.salnikov@gmail.com>
2024-06-16 15:57:57 +0000

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,26 @@
+/*
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.6.7/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.6.7/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyA6CX_eaJy1_wKTw6UM2BxH5-2J1_pt4H8",
+    authDomain: "maildoc.firebaseapp.com",
+    databaseURL: "https://maildoc.firebaseio.com",
+    projectId: "maildoc",
+    storageBucket: "maildoc.appspot.com",
+    messagingSenderId: "276717161036",
+    appId: "1:276717161036:web:b2dfdf5846a2c91d6042e9",
+    measurementId: "G-6W6JTCQG9C"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+*/
